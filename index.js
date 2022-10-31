b8a73d1ad1d37000469c0c55f6e53e86f72473ad
vankhanh-ctu <88043928+vankhanh-ctu@users.noreply.github.com>
2022-10-31 22:37:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
-//import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
-//import { getDatabase, ref, set, child, get } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js";
-import { initializeApp, getDatabase, ref, set, child, get } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
+import { getDatabase, ref, set, child, get } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
